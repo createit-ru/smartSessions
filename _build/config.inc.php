f742a710d700863832b73391a8d81d1f9be09c0f
Alexey Naumov <info@createit.ru>
2021-08-02 09:48:22 +0300

    Версия 1.0.2-pl

--- _build/config.inc.php
+++ _build/config.inc.php
@@ -11,7 +11,7 @@ if (!defined('MODX_CORE_PATH')) {
 return [
     'name' => 'smartSessions',
     'name_lower' => 'smartsessions',
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'release' => 'pl',
     // Install package to site right after build
     'install' => true,
